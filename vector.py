9e81274ce3314105f9d40f5eda236fef66762a4a
ciancgiu <giulesciancaglini@gmail.com>
2025-04-03 22:10:46 -0400

    update

--- vector.py
+++ vector.py
@@ -1,6 +1,7 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 import pandas as pd
 from langchain_community.document_loaders import PyPDFLoader
@@ -8,34 +9,37 @@ import shutil
 
 pdf_path = "dsa.pdf"
 loader = PyPDFLoader(pdf_path)
-content = loader.load_and_split
+content = loader.load()
 embeddings = OllamaEmbeddings(model = "mxbai-embed-large")
 
 db_location = "./chroma_langchain_db"
 add_documents = not os.path.exists(db_location)
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunks_size=800,
+    overlap=80,
+    length_function = len,
+    is_separator_regex=False
+)
+chunks = text_splitter.split_text(content)
+
 if add_documents:
     documents = []
-    ids = []
 
-    for i, page in enumerate(content):
-        document = Document(
+    for i, chunk in enumerate(chunks):
             document = Document(
-                page_content=page,
-                metadata={"page_number": i+1},
-                id = str(i)
-            )     
-        )
-        ids.append(str(i))
-        documents.append(document)
+                page_content=chunk,
+                metadata={"chunk": i},
+            )    
+    documents.append(document)
 
 vector_store = Chroma(
-    collection_name = "pdf_pages",
+    collection_name = "pdf_chunks",
     persist_directory=db_location,
     embedding_function=embeddings
 )
 if add_documents:
-    vector_store.add_documents(documents=documents,ids = ids)
+    vector_store.add_documents(documents=documents)
 
 retriever = vector_store.as_retriever(
     search_kwargs = {"k":5}
